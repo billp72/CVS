2724f20f64e2a376b5301e3710e25cd2418f3f77
netcreative <epop.bill@gmail.com>
2017-01-14 10:56:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+CVS demo for 1/16 interview
+
+* running on port 3000
+* db name Books
+
+node/mongo/mongoose
---
+++ index.js
@@ -0,0 +1,61 @@
+//
+var express = require('express');
+var app = express();
+var parseString = require('xml2js').parseString;
+var fs = require('fs');
+//var MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+var assert = require('assert');
+var url = 'mongodb://127.0.0.1/Books';
+//var xml = "<root>Item one</root>";
+mongoose.connect(url);
+
+app.use('/', express.static(__dirname + '/public')); // ← adjust
+
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+    author: String
+    /*title: String,
+    genre: String,
+    price: String,
+    publish_date: String,
+    description: String*/
+});
+
+var Book = mongoose.model('Book', bookSchema);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+// index page 
+app.get('/', function(req, res, next) {
+	
+	fs.readFile(__dirname + '/items.xml', function(err, xml) {
+		parseString(xml, function (err, result) {
+
+    		res.render('pages/index', {data: result.catalog.book});
+
+    		var book = new Book({ author:  result.catalog.book[0].author[0]});
+
+    		book.save(function(err, author){})
+    		/*Book.collection.insertMany(result.catalog.book, function(err,r) {
+      			assert.equal(null, err);
+      			assert.equal(12, r.insertedCount);
+
+      			db.close();
+			})*/
+			
+		});
+  	});
+	
+});
+
+// about page 
+/*app.get('/about', function(req, res) {
+    res.render('pages/about');
+});*/
+
+app.listen(3000, function() { console.log('listening'); });
+
